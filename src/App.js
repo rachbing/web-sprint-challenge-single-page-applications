4ccd0452de7867a23a689a002b7f03e67a0693fe
Rachel Bingham <rachelbingham@Rachels-MacBook-Air.local>
2022-04-28 22:55:37 -0600

    first push, started app and made components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Home from "./Components/Home";
+import Order from "./Components/Order";
+
+import axios from "axios";
+
+import { Route, Link, Switch } from "react-router-dom";
+
+console.log("hello")
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  toppings: false,
+  special: ''
+}
+
+const initialFormErrors = {
+  name: '',
+  size: '',
+  toppings: false,
+  special:''
+}
 
 const App = () => {
+  const [order, setOrder] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState("");
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue});
+  }
+
+  const handleSubmit = () => {
+    axios.post('https://reqres.in/api/orders', formValues)
+      .then(res => {
+        setOrder([ res.data, ...order])
+      })
+      .catch(err => console.error(err))
+      .finally(() => setFormValues(initialFormValues))
+  }
+
   return (
     <>
       <h1>Lambda Eats</h1>
---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+import { useHistory } from 'react-router-dom'
+
+const Home = (props) => {
+
+    const history = useHistory();
+
+    const routeToOrder = () => {
+        history.push("/pizza");
+    }
+
+    return (
+        <h1> Make a pizza with us! </h1>
+    )
+}
